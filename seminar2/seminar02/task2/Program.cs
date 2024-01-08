6a200c16f82238a3eca2c22491a4a02204eeffec
Natali Tremasova <n.resova@yandex.ru>
2024-01-08 16:13:38 +0300

    add task2 answer

--- seminar2/seminar02/task2/Program.cs
+++ seminar2/seminar02/task2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//2. Определить номер координатной плоскости, которой принадлежит точка с координатами (x,y)
+double x = +3.3;
+double y = -6.8;
+if (x > 0)
+{
+   if (y > 0)
+    {
+       Console.WriteLine($"I");
+    }  
+   else
+   {   
+       Console.WriteLine($"IV");
+   }
+}
+if (x < 0)
+ {
+if (y > 0)
+  {
+      Console.WriteLine($"II");
+   }
+  else
+   {   
+       Console.WriteLine($"III");
+   }
+ }
