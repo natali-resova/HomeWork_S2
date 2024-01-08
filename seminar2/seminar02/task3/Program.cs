a3a6bed7d55f8834564ba2dac016a463db65adc3
Natali Tremasova <n.resova@yandex.ru>
2024-01-08 16:25:10 +0300

    add task3 answer

--- seminar2/seminar02/task3/Program.cs
+++ seminar2/seminar02/task3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//3.Показать наибольшую цифру целого числа, принадлежащего отрезку [10, 99]
+//D=d1*10+d2
+int D = 99;
+int d1 = D / 10;
+int d2 = D - d1*10;
+if (d1 > d2)
+{
+   Console.WriteLine(d1);
+}
+else
+{
+   if (d1 < d2)
+   {
+      Console.WriteLine(d2);
+   }
+   else
+   {
+       Console.WriteLine($"{d1}{d2} - количество десятков и единиц в этом числе одинаково");
+   }
+}
